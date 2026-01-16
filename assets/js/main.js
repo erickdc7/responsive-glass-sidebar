90b962b6cdd43865c3a92bca562615aa26d45274
Erick Díaz <erickdiazc717@gmail.com>
2026-01-16 11:17:25 -0500

    add listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@
 const showSidebar = (toggleId, sidebarId) => {
     const toggle = document.getElementById(toggleId),
         sidebar = document.getElementById(sidebarId)
+
+    toggle.addEventListener('click', () => {
+        
+    })
 }
 
 showSidebar('header-toggle', 'sidebar')
